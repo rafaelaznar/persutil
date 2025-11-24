252904bee4e2d0a5cca6bef993d57fe1fe53df3e
marcos <marcospp172006@gmail.com>
2025-11-24 21:54:21 +0100

    cambios para integrar Pallas API

--- src/main/java/net/ausiasmarch/persutil/entity/PallasEntity.java
+++ src/main/java/net/ausiasmarch/persutil/entity/PallasEntity.java
@@ -1,32 +1,40 @@
 package net.ausiasmarch.persutil.entity;
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "notas")
+@Table(name = "pallas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PallasEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private long id;
     @NotNull
-    @Size(min = 3, max = 255)
+    @Size(min = 3, max = 1024)
     private String titulo;
-
     @NotNull
     @Size(min = 3)
     private String contenido;
-    
     @NotNull
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss ", shape = JsonFormat.Shape.STRING)
-    private Date fechaCreacion;
-    
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime fechaCreacion;
+    @Nullable
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime fechaModificacion;
     
 }
