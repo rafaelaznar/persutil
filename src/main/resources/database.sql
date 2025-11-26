d0261196c8714d7c887c7c02d81854b78b81b022
Lucia Castanera <castaneralucia@gmail.com>
2025-11-26 12:12:50 +0100

    tabla castanyera añadida

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: database:3306
--- Tiempo de generación: 20-11-2025 a las 10:36:08
--- Versión del servidor: 8.4.6
--- Versión de PHP: 8.2.29
+-- Tiempo de generación: 10-11-2025 a las 11:28:03
+-- Versión del servidor: 8.4.5
+-- Versión de PHP: 8.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -23,29 +23,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `blog` (
   `id` bigint NOT NULL,
-  `titulo` varchar(1024) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
-  `contenido` longtext CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
-  `etiquetas` varchar(1024) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `titulo` varchar(1024) COLLATE utf32_unicode_ci NOT NULL,
+  `contenido` longtext COLLATE utf32_unicode_ci NOT NULL,
+  `etiquetas` varchar(1024) COLLATE utf32_unicode_ci NOT NULL,
   `fecha_creacion` datetime NOT NULL,
   `fecha_modificacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `castanyera`
---
-
-CREATE TABLE `castanyera` (
-  `id` bigint NOT NULL,
-  `titulo` varchar(255) NOT NULL,
-  `comentario` longtext NOT NULL,
-  `fecha_creación` datetime NOT NULL,
-  `fecha_modificación` datetime NOT NULL,
-  `publico` tinyint(1) NOT NULL,
-  `imagen` varchar(255) NOT NULL,
-  `etiquetas` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Índices para tablas volcadas
@@ -58,6 +41,38 @@ ALTER TABLE `blog`
   ADD PRIMARY KEY (`id`);
 
 --
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `castanyera`
+--
+
+CREATE TABLE `castanyera` (
+  `id` bigint NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `contenido` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `fecha_creación` datetime NOT NULL,
+  `fecha_modificación` datetime NOT NULL,
+  `publico` tinyint(1) NOT NULL,
+  `etiquetas` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
 -- Indices de la tabla `castanyera`
 --
 ALTER TABLE `castanyera`
@@ -66,12 +81,6 @@ ALTER TABLE `castanyera`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `blog`
---
-ALTER TABLE `blog`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT de la tabla `castanyera`
