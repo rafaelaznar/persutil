7dcb25b5636756e335a10e140c6c1e570c5088ca
Alvaro Contreras <alvaroch975@gmail.com>
2025-12-01 11:54:44 +0100

    v6 -> Final

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -104,28 +104,59 @@ CREATE TABLE `castanyera` (
 --
 
 CREATE TABLE `contreras` (
-  `id` bigint(20) NOT NULL,
-  `titulo` varchar(1024) NOT NULL,
-  `contenido` varchar(255) NOT NULL,
-  `etiquetas` varchar(1024) NOT NULL,
+  `id` bigint NOT NULL,
+  `titulo` varchar(1024) COLLATE utf32_unicode_ci NOT NULL,
+  `contenido` longtext COLLATE utf32_unicode_ci NOT NULL,
+  `etiquetas` varchar(1024) COLLATE utf32_unicode_ci NOT NULL,
   `fecha_creacion` datetime NOT NULL,
   `fecha_modificacion` datetime DEFAULT NULL,
-  `publico` tinyint(1) NOT NULL,
-  `imagenUrl` varchar(512) DEFAULT NULL
+  `publico` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `contreras`
 --
 
-INSERT INTO `contreras` (`id`, `titulo`, `contenido`, `etiquetas`, `fecha_creacion`, `fecha_modificacion`, `publico`, `imagenUrl`) VALUES
-(1, 'Clase 1-B', 'EXAMEN MATES LUNES', 'clase,examen', '2025-11-28 03:17:48', '2025-11-28 03:19:12', 1, NULL),
-(2, 'Excursion', 'proxima excursion al museo el jueves :D', 'salida,museo', '2025-11-28 03:17:48', '2025-11-28 11:28:24', 0, NULL),
-(3, 'Reunion padres', 'reunion padres viernes a las 17h', 'reunion,familiar', '2025-11-28 03:17:48', NULL, 1, NULL),
-(4, 'Biblioteca', 'nuevos libros disponibles en biblioteca, 10/10', 'libros,biblioteca', '2025-11-28 03:17:48', '2025-11-28 03:19:35', 0, NULL),
-(5, 'Educacion fisica', 'traer ropa deportiva el martes', 'deporte,clase', '2025-11-28 03:17:48', NULL, 1, NULL),
-(6, 'Comedor escolar', 'menu especial sin gluten esta semana', 'comedor,menu', '2025-11-28 03:17:48', NULL, 1, NULL);
+INSERT INTO `contreras` (`id`, `titulo`, `contenido`, `etiquetas`, `fecha_creacion`, `fecha_modificacion`, `publico`) VALUES
+(3, 'Examen de mates', 'Examen de matematicas el lunes para 4B', 'examen,4B', '2025-12-01 10:49:02', NULL, 0),
+(4, 'Horarios', 'Publicacion de los nuevos horarios del trimestre', 'horarios', '2025-12-01 10:49:02', NULL, 1),
+(5, 'Reunion padres', 'Reunion general de padres el jueves a las 18:00', 'reunion,padres', '2025-12-01 10:49:02', NULL, 0),
+(6, 'Actividad deportiva', 'Inscripciones abiertas para actividades deportivas', 'deportes', '2025-12-01 10:49:02', NULL, 1),
+(7, 'Salida escolar', 'Salida escolar el viernes, llevar autorizacion firmada', 'salida,4B', '2025-12-01 10:49:02', NULL, 0),
+(8, 'tocame', 'estoy editado', 'hola, si, no', '2025-12-01 11:49:51', '2025-12-01 11:49:58', 1);
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `contreras`
+--
+ALTER TABLE `contreras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contreras`
+--
+ALTER TABLE `contreras`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
 -- --------------------------------------------------------
 
 --
